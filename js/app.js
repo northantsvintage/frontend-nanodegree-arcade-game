ec924307aceedfbddc776c5c7e7629ead9e9cef6
Aida Papic <northantsvintage@gmail.com>
2018-05-05 13:43:27 +0100

    fixed up arrow

--- js/app.js
+++ js/app.js
@@ -1,87 +1,16 @@
-// *********** ENEMY **************
-
-// Enemies our player must avoid
+/** *********** ENEMY **************
+* Enemies our player must avoid
+*/
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    // movement coordinates
     this.x = x;
     this.y = y;
     this.speed = 100 + Math.floor(Math.random() * 250);
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// *********** PLAYER **************
-
-// Now write your own player class
-var Player = function(){
-    this.x = 200;
-    this.y = 400;
-    this.sprite = 'images/char-boy.png';
-}
-
-
-
-// Win Game
-// Make the win popup and overlay appear
-function winGame() {
-  const OVERLAY = document.getElementById('overlay');
-  OVERLAY.classList.remove('hidden');
-
-  const WIN = document.getElementById('win');
-  WIN.classList.remove('hidden');
-}
-
-// Restart the game after winning if click on the restart button
-const RESTART = document.getElementById('restart');
-
-RESTART.addEventListener('click', function() {
-  location.reload();
-});
-
-
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-Player.prototype.update = function() {
-        if (this.y <= 50) {
-        this.x = 202;
-        this.y = 480;
-        this.sprite = 'images/char-boy.png';
-    }
-
-}
-
-Player.prototype.handleInput = function(event) {
-    console.log(event.type);
-    if (event === 'right' && this.x < 400){
-        this.x += 100;
-    } else if(event === 'left' && this.x >99){
-        this.x -= 100;
-    } else if(event === 'up' && this.y === 480){
-        this.y -= 50;
-    } else if(event === 'up' && this.y > 90){
-        this.y -= 85;
-    }else if(event === 'up' && this.y === 90){
-        this.score += 100;
-        this.y = 620;
-        this.x = 200;
-    } else if (event === 'down' && this.y < 400){
-        if (this.y === 430){
-            this.y += 50;
-        } else{
-            this.y += 85;
-        }
-    }
-}
-
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/** Update the Enemy's position, required method for game
+* Parameter: dt, a time delta between ticks
+*/
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -90,20 +19,19 @@ Enemy.prototype.update = function(dt) {
         this.x = this.x + (this.speed * dt);
     } else {
         this.x = -this.x;
-    }
+    };
 
-        //If the player comes within 10px of an enemy's x and y coordinates, reset the game
-        if(player.x >= this.x - 10 && player.x <= this.x + 10){
-            if(player.y >= this.y - 10 && player.y <= this.y + 10){
-                player.x = 200;
-                player.y = 400;
-            }
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30) {
+        if(player.y >= this.y - 30 && player.y <= this.y + 30) {
+            player.x = 200;
+            player.y = 400;
         }
-
-        if (player.y <= 100) {
-            winGame();
-        }
-
+    };
+    // if player reaches the water, it wins
+    if (player.y < 80) {
+        setTimeout(youWon(),500);
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -111,13 +39,50 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// // Place the player object in a variable called player
+/** *********** PLAYER **************
+* Now write your own player class
+*/
+var Player = function() {
+    this.x = 200;
+    this.y = 400;
+    this.sprite = 'images/char-princess-girl.png';
+};
+// shows sprite on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// if Player reaches the water, Player returned in the starting position
+Player.prototype.update = function() {
+        if (this.y <= 0) {
+        this.x = 200;
+        this.y = 400;
+    };
+};
+
+/** Player position changes 100px up or down or left or right
+* depending on the arrow keys
+*/
+Player.prototype.handleInput = function(event) {
+    if( event === 'left' && this.x > 0 )  
+        this.x -= 100;
+    else if( event === 'right' && this.x < 400)
+        this.x += 100;
+    else if( event === 'up' && this.y > 0)
+        this.y -= 80;
+    else if( event === 'down' && this.y < 400)
+        this.y += 80;
+};
+
+/** *********** NEW PLAYER AND ENEMY OBJECTS ************** 
+* Instantiated Player and Enemy Objects
+* Enemys pushed into array
+*/
 var player = new Player();
-// Now instantiate your objects.
+
 var enemy1 = new Enemy(-150, 50);
 var enemy2 = new Enemy(-100, 140);
 var enemy3 = new Enemy(-50, 230);
-// Place all enemy objects in an array called allEnemies
+
 var allEnemies = [];
 (function() {
     allEnemies.push(enemy1);
@@ -125,8 +90,28 @@ var allEnemies = [];
     allEnemies.push(enemy3);
 })();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/** WINNING
+* Win popup and overlay appear when player reaches the water
+*/
+function youWon() {
+    const overlay = document.getElementById('overlay');
+    overlay.classList.remove('hidden');
+    const won = document.getElementById('winner');
+    won.classList.remove('hidden');
+  }
+
+/** RESTART
+* Restart the game after winning
+*/
+const restart = document.getElementById('restart');
+  
+restart.addEventListener('click', function() {
+  location.reload();
+});  
+
+/** GAME CONTROL
+* This listens for key presses and sends the keys to your Player.handleInput()
+*/
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -136,6 +121,4 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
-
-
+}); 
